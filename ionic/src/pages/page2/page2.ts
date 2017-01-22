b4931beafa39618fe059fd41d081c5c495f1981d
James Talarico <james.v.talarico@gmail.com>
2017-01-21 19:56:04 -0500

    Set up image sending

--- ionic/src/pages/page2/page2.ts
+++ ionic/src/pages/page2/page2.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { ChallengeService } from '../../providers/challenge-service';
+import { ImageService } from '../../providers/image-service';
+
 
 // Import camera
 import {Camera} from 'ionic-native';
@@ -10,7 +12,7 @@ import {Camera} from 'ionic-native';
 @Component({
   selector: 'page-page2',
   templateUrl: 'page2.html',
-  providers: [ChallengeService]
+  providers: [ChallengeService, ImageService]
 })
 export class Page2 {
   selectedItem: any;
@@ -20,24 +22,26 @@ export class Page2 {
   public challenge: string;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public challengeService: ChallengeService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public challengeService: ChallengeService, public imageService: ImageService) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
     this.loadChallenge;
-
   }
 
   public takePicture(){
     Camera.getPicture({
-      destinationType: Camera.DestinationType.FILE_URI,
+      destinationType: Camera.DestinationType.DATA_URL,
       targetWidth: 1000,
       targetHeight: 1000
     }).then((imageData) => {
       // imageData is a base64 encoded string
-      this.base64Image = imageData;
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+      this.sendPhoto(this.base64Image);
     }, (err) => {
       console.log(err);
     });
+
+
   }
 
   public loadChallenge() {
@@ -48,5 +52,14 @@ export class Page2 {
 
       })
   }
+
+  public sendPhoto(image) {
+
+    this.imageService.post(image)
+      .then(data => {
+      })
+  }
+
+
 
 }
